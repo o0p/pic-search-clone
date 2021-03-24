b73c38539eebc11eb257f8797545e98543791440
Russell <russelldev@protonmail.com>
2021-03-24 23:51:03 +0000

    Rewrite onSearchSubmit in to arrow func

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import axios from 'axios';
-import SearchBar from './SearchBar';
-import ImageList from './ImageList';
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
   state = { images: [] };
 
-  async onSearchSubmit(term) {
-    const res = await axios.get('https://api.unsplash.com/search/photos', {
+  onSearchSubmit = async (term) => {
+    const res = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
-        Authorization: 'Client-ID 7wNJ_4HIGqQrt4UlFj7uxVlkSOkCcUTjPobwwk9kqMI',
+        Authorization: "Client-ID 7wNJ_4HIGqQrt4UlFj7uxVlkSOkCcUTjPobwwk9kqMI",
       },
     });
-    console.log(res.data.results);
-  }
+
+    this.setState({ images: res.data.result });
+  };
 
   render() {
     return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
+      <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images} />
+        Found: {this.state.images.length} inmages
+        {/* <ImageList images={this.state.images} /> */}
       </div>
     );
   }

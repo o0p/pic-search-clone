43c2627010567130165efb0d12e0cde47ff583fb
Russell <russelldev@protonmail.com>
2021-03-24 23:59:14 +0000

    Add base URL

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,1 +1,8 @@
 import axios from "axios";
+
+axios.create({
+    baseURL: 'https://api.unsplash.com'
+    headers: {
+        Authorization: "Client-ID 7wNJ_4HIGqQrt4UlFj7uxVlkSOkCcUTjPobwwk9kqMI",
+    },
+});

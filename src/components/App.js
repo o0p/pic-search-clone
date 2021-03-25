4a695a3d0b2d203b5d19dd03e643f311ba28492d
Russell <russelldev@protonmail.com>
2021-03-25 00:14:40 +0000

    Add ImageList

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
@@ -7,7 +7,7 @@ class App extends React.Component {
   state = { images: [] };
 
   onSearchSubmit = async (term) => {
-    const res = await axios.get('/search/photos', {
+    const res = await unsplash.get('/search/photos', {
       params: { query: term },
     });
 
@@ -18,8 +18,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} inmages
-        {/* <ImageList images={this.state.images} /> */}
+        <ImageList images={this.state.images} />
       </div>
     );
   }
